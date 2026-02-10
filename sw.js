29d6bd38135289d6150063db162f1fd98f3e13c1
muvarna <biblioteka.mu.varna@gmail.com>
2026-02-10 10:59:57 +0200

    Update service worker cache version and management
    Updated CACHE_NAME to 'chirpsynth-v1.3' and added cache management in the activate event.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'chirpsynth-v1.2';
+const CACHE_NAME = 'chirpsynth-v1.3';
 const ASSETS = [
   './',
   './index.html',
@@ -16,9 +16,24 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting(); // Force the new service worker to take over immediately
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
